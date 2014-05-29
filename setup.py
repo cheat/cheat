0a3fd3559c74b501e630a8a841285af91e0a5b82
Chris Lane <chris@chris-allen-lane.com>
2014-05-28 20:47:49 -0400

    Resolves #160
    The `setup.py` script no longer attempts to install files to privileged
    system directories. (Previously, it attempted to do this in order to
    enable command-line autocompletion.) In lieu of doing this within the
    installer directly, I have simply included brief instructions explaining
    how to configure this manually.
    
    Version bumped accordingly.

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 from distutils.core import setup
 import os
 
-data = [
-       ('/usr/share/zsh/site-functions', ['cheat/autocompletion/_cheat.zsh']),
-       ('/etc/bash_completion.d'       , ['cheat/autocompletion/cheat.bash']),
-       ('/usr/share/fish/completions'  , ['cheat/autocompletion/cheat.fish'])
-       ]
-
-if os.name == 'nt':
-    data = []
-
 setup(
     name         = 'cheat',
-    version      = '2.0.5',
+    version      = '2.0.6',
     author       = 'Chris Lane',
     author_email = 'chris@chris-allen-lane.com',
     license      = 'GPL3',
@@ -29,8 +20,7 @@ setup(
     package_data = {
         'cheat.cheatsheets': [f for f in os.listdir('cheat/cheatsheets') if '.' not in f]
     },
-    scripts      = ['bin/cheat'],
-    data_files   = data,
+    scripts          = ['bin/cheat'],
     install_requires = [
         'docopt >= 0.6.1',
         'pygments >= 1.6.0',
