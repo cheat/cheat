dde3acf109d48d0cc25213e14e1582169c68a160
Erik Cox <erikbcox@gmail.com>
2014-09-13 15:46:57 -0700

    Added a condition in prompt_yes_or_no() to use input or raw_input

--- cheat/utils.py
+++ cheat/utils.py
@@ -48,8 +48,16 @@ def editor():
 
 def prompt_yes_or_no(question):
     """ Prompts the user with a yes-or-no question """
+    # Support Python 2 and 3 input
+    # Default to Python 2's input()
+    get_input = raw_input
+ 
+    # If this is Python 3, use input()
+    if sys.version_info[:2] >= (3, 0):
+        get_input = input
+
     print(question)
-    return raw_input('[y/n] ') == 'y'
+    return get_input('[y/n] ') == 'y'
 
 
 def warn(message):
