be2cb6e6320a4507562ee5ed5c45978ce86a5b29
Chris Lane <chris@chris-allen-lane.com>
2015-01-05 20:44:25 -0500

    Version 2.1.3
    - Added new cheatsheets
    - Appended to `.gitignore`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name         = 'cheat',
-    version      = '2.1.2',
+    version      = '2.1.3',
     author       = 'Chris Lane',
     author_email = 'chris@chris-allen-lane.com',
     license      = 'GPL3',
