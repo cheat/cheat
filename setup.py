a8f91d4224b623ad2edc1fa403700edd7fea471d
Chris Lane <chris@chris-allen-lane.com>
2015-10-15 18:37:02 -0400

    Minor tweaks.

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 """cheat
-    ~~~~~~~~
-
-    cheat allows you to create and view interactive cheatsheets on the
-    command-line. It was designed to help remind *nix system administrators of
-    options for commands that they use frequently, but not frequently enough
-    to remember.
-    :license: GPL3
+~~~~~~~~
+cheat allows you to create and view interactive cheatsheets on the
+command-line. It was designed to help remind *nix system administrators of
+options for commands that they use frequently, but not frequently enough
+to remember.
+:license: GPL3
 """
 
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'cheat',
-    version      = '2.1.16',
-    author       = 'Chris Lane',
-    author_email = 'chris@chris-allen-lane.com',
-    license      = 'GPL3',
-    description  = 'cheat allows you to create and view interactive cheatsheets on the command-line',
+    name             = 'cheat',
+    version          = '2.1.16',
+    author           = 'Chris Lane',
+    author_email     = 'chris@chris-allen-lane.com',
+    license          = 'GPL3',
+    description      = 'cheat allows you to create and view interactive cheatsheets on the command-line',
     long_description = __doc__,
-    url          = 'https://github.com/chrisallenlane/cheat',
-    packages     = find_packages(),
-    package_data = {
+    url              = 'https://github.com/chrisallenlane/cheat',
+    packages         = find_packages(),
+    package_data     = {
         'cheat.cheatsheets': ['*'],
     },
     entry_points = {
