20d52376f9b82d9cc193955961e45bc51a9ba166
Chris Lane <chris@chris-allen-lane.com>
2015-10-15 18:56:34 -0400

    Undid packaging refactoring.

--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
-"""cheat
-~~~~~~~~
-cheat allows you to create and view interactive cheatsheets on the
-command-line. It was designed to help remind *nix system administrators of
-options for commands that they use frequently, but not frequently enough
-to remember.
-:license: GPL3
-"""
-
-from setuptools import setup, find_packages
+from distutils.core import setup
+import os
 
 setup(
-    name             = 'cheat',
-    version          = '2.1.17',
-    author           = 'Chris Lane',
-    author_email     = 'chris@chris-allen-lane.com',
-    license          = 'GPL3',
-    description      = 'cheat allows you to create and view interactive cheatsheets on the command-line',
-    long_description = __doc__,
-    url              = 'https://github.com/chrisallenlane/cheat',
-    packages         = find_packages(),
-    package_data     = {
-        'cheat.cheatsheets': ['*'],
+    name         = 'cheat',
+    version      = '2.1.16',
+    author       = 'Chris Lane',
+    author_email = 'chris@chris-allen-lane.com',
+    license      = 'GPL3',
+    description  = 'cheat allows you to create and view interactive cheatsheets '
+    'on the command-line. It was designed to help remind *nix system '
+    'administrators of options for commands that they use frequently, but not '
+    'frequently enough to remember.',
+    url          = 'https://github.com/chrisallenlane/cheat',
+    packages     = [
+        'cheat',
+        'cheat.cheatsheets',
+        'cheat.test',
+    ],
+    package_data = {
+        'cheat.cheatsheets': [f for f in os.listdir('cheat/cheatsheets') if '.' not in f]
     },
-    entry_points = {
-        'console_scripts': [
-            'cheat = cheat.app:main',
-        ],
-    },
+    scripts          = ['bin/cheat'],
     install_requires = [
         'docopt >= 0.6.1',
         'pygments >= 1.6.0',
