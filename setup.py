20d985d99326cb21170627e3c4af4f8c7f6158c6
Chris Lane <chris@chris-allen-lane.com>
2016-03-24 12:41:40 -0400

    Working around failed upload
    pypi responded with an HTTP 500 when uploading the last version of
    `cheat`, and this seems to have caused some problems. No changes were
    made in this commit other than incrementing the version number in an
    attempt to resolve this.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name         = 'cheat',
-    version      = '2.1.23',
+    version      = '2.1.24',
     author       = 'Chris Lane',
     author_email = 'chris@chris-allen-lane.com',
     license      = 'GPL3',
