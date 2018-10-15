8ec51d31944853a31bc41030a2112509f6adcd02
Chris Lane <chris@chris-allen-lane.com>
2018-10-15 10:51:11 -0400

    v2.3.0
    Version-bump to `v2.3.0`.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name         = 'cheat',
-    version      = '2.2.3',
+    version      = '2.3.0',
     author       = 'Chris Lane',
     author_email = 'chris@chris-allen-lane.com',
     license      = 'GPL3',
