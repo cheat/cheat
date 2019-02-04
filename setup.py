074dba6e99c91f6c18b4b3a841bd00d600f1c0b3
Chris Lane <chris@chris-allen-lane.com>
2019-02-04 12:04:52 -0500

    v2.5.0
    `minor` version bump to `2.5.0`.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for f in os.listdir('cheat/cheatsheets/'):
 # specify build params
 setup(
     name='cheat',
-    version='2.4.2',
+    version='2.5.0',
     author='Chris Lane',
     author_email='chris@chris-allen-lane.com',
     license='GPL3',
