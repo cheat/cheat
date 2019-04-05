7b2a28fc97e975f5f6786891c5ee31ad31f5a14a
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-04-05 16:03:06 +0900

    print instructive message instead of error message after `cheat -l` when there is no cheetsheet
    If there's no cheatsheets,
    ```bash
    ValueError: max() arg is an empty sequence
    ```
    is thrown while calculating `pad_length` in function `list` in cheat/sheets.py
    
    This commit prints a message
    ```bash
    No cheatsheets yet. Create and edit by `cheat -e <your-cheatsheet>`.
    ```
    instead of the raw error message.

--- cheat/sheets.py
+++ cheat/sheets.py
@@ -55,6 +55,8 @@ class Sheets:
 
     def list(self):
         """ Lists the available cheatsheets """
+        if not len(self.get()):
+            return 'No cheatsheets yet. Create and edit by `cheat -e <your-cheatsheet>`.'
         sheet_list = ''
         pad_length = max([len(x) for x in self.get().keys()]) + 4
         for sheet in sorted(self.get().items()):
