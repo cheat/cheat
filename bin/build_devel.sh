a13ad992416c9054ff078bcf12a7cab721de01a9
Chris Lane <chris@chris-allen-lane.com>
2019-10-28 19:39:22 -0400

    chore: modified build scripts
    Modified build scripts to always use local vendoring, now that the
    project contains a `vendor` directory.

--- bin/build_devel.sh
+++ bin/build_devel.sh
@@ -6,7 +6,7 @@ APPDIR=$(readlink -f "$BINDIR/..")
 
 # compile the executable
 cd "$APPDIR/cmd/cheat"
-go clean && go generate && go build
+go clean && go generate && go build -mod vendor
 mv "$APPDIR/cmd/cheat/cheat" "$APPDIR/dist/cheat"
 
 # display a build checksum
--- bin/build_release.sh
+++ bin/build_release.sh
@@ -9,6 +9,11 @@ cd "$APPDIR/cmd/cheat"
 go clean && go generate
 
 # compile AMD64 for Linux, OSX, and Windows
-env GOOS=darwin  GOARCH=amd64 go build -o "$APPDIR/dist/cheat-darwin-amd64"  "$APPDIR/cmd/cheat"
-env GOOS=linux   GOARCH=amd64 go build -o "$APPDIR/dist/cheat-linux-amd64"   "$APPDIR/cmd/cheat"
-env GOOS=windows GOARCH=amd64 go build -o "$APPDIR/dist/cheat-win-amd64.exe" "$APPDIR/cmd/cheat"
+env GOOS=darwin  GOARCH=amd64 go build -mod vendor -o \
+  "$APPDIR/dist/cheat-darwin-amd64"  "$APPDIR/cmd/cheat"
+
+env GOOS=linux   GOARCH=amd64 go build -mod vendor -o \
+  "$APPDIR/dist/cheat-linux-amd64"   "$APPDIR/cmd/cheat"
+
+env GOOS=windows GOARCH=amd64 go build -mod vendor -o \
+  "$APPDIR/dist/cheat-win-amd64.exe" "$APPDIR/cmd/cheat"
