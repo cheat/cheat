27a4991a3adec347579890024ee8353f83c50ca8
Chris Lane <chris@chris-allen-lane.com>
2019-11-12 18:59:37 -0500

    feat: updates build scripts
    Build scripts now invoke `go mod vendor` and `go mod tidy` early in the
    build process, to ensure that the `vendor` directory stays consistent
    with the `go.mod` et. al.

--- bin/build_devel.sh
+++ bin/build_devel.sh
@@ -4,6 +4,9 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 APPDIR=$(readlink -f "$BINDIR/..")
 
+# update the vendored dependencies
+go mod vendor && go mod tidy
+
 # compile the executable
 cd "$APPDIR/cmd/cheat"
 go clean && go generate && go build -mod vendor
--- bin/build_release.sh
+++ bin/build_release.sh
@@ -4,6 +4,9 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 APPDIR=$(readlink -f "$BINDIR/..")
 
+# update the vendored dependencies
+go mod vendor && go mod tidy
+
 # build embeds
 cd "$APPDIR/cmd/cheat"
 go clean && go generate
