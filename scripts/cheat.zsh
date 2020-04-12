477650ee441656c86b1d5f890158b043c2490343
Shohei YOSHIDA <syohex@gmail.com>
2020-04-12 11:03:08 +0900

    Improve zsh completion

--- scripts/cheat.zsh
+++ scripts/cheat.zsh
@@ -2,9 +2,15 @@
 
 local cheats taglist pathlist
 
-_cheat_complete_cheatsheets()
+_cheat_complete_personal_cheatsheets()
 {
   cheats=("${(f)$(cheat -l -t personal | tail -n +2 | cut -d' ' -f1)}")
+  _describe -t cheats 'cheats' cheats
+}
+
+_cheat_complete_full_cheatsheets()
+{
+  cheats=("${(f)$(cheat -l | tail -n +2 | cut -d' ' -f1)}")
   _describe -t cheats 'cheats' cheats
 }
 
@@ -26,7 +32,7 @@ _cheat() {
     '(--init)--init[Write a default config file to stdout]: :->none' \
     '(-c --colorize)'{-c,--colorize}'[Colorize output]: :->none' \
     '(-d --directories)'{-d,--directories}'[List cheatsheet directories]: :->none' \
-    '(-e --edit)'{-e,--edit}'[Edit <sheet>]: :->full' \
+    '(-e --edit)'{-e,--edit}'[Edit <sheet>]: :->personal' \
     '(-l --list)'{-l,--list}'[List cheatsheets]: :->full' \
     '(-p --path)'{-p,--path}'[Return only sheets found on path <name>]: :->pathlist' \
     '(-r --regex)'{-r,--regex}'[Treat search <phrase> as a regex]: :->none' \
@@ -34,13 +40,17 @@ _cheat() {
     '(-t --tag)'{-t,--tag}'[Return only sheets matching <tag>]: :->taglist' \
     '(-T --tags)'{-T,--tags}'[List all tags in use]: :->none' \
     '(-v --version)'{-v,--version}'[Print the version number]: :->none' \
-    '(--rm)--rm[Remove (delete) <sheet>]: :->full' \
+    '(--rm)--rm[Remove (delete) <sheet>]: :->personal' \
+    '(-)*: :->full'
 
   case $state in
     (none)
       ;;
     (full)
-      _cheat_complete_cheatsheets
+      _cheat_complete_full_cheatsheets
+      ;;
+    (personal)
+      _cheat_complete_personal_cheatsheets
       ;;
     (taglist)
       _cheat_complete_tags
@@ -49,7 +59,6 @@ _cheat() {
       _cheat_complete_paths
       ;;
     (*)
-      _cheat_complete_cheatsheets
       ;;
   esac
 }
