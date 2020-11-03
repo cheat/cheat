0b21ccf6f851a5d450efd48e2c3ec2fd6df18cda
Chris Lane <chris@chris-allen-lane.com>
2020-11-02 21:41:05 -0500

    feat(tests): improve test coverage

---
+++ internal/config/init_test.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+// TestInit asserts that configs are properly initialized
+func TestInit(t *testing.T) {
+
+	// initialize a temporary config file
+	confFile, err := ioutil.TempFile("", "cheat-test")
+	if err != nil {
+		t.Errorf("failed to create temp file: %v", err)
+	}
+
+	// clean up the temp file
+	defer os.Remove(confFile.Name())
+
+	// initialize the config file
+	conf := "mock config data"
+	if err = Init(confFile.Name(), conf); err != nil {
+		t.Errorf("failed to init config file: %v", err)
+	}
+
+	// read back the config file contents
+	bytes, err := ioutil.ReadFile(confFile.Name())
+	if err != nil {
+		t.Errorf("failed to read config file: %v", err)
+	}
+
+	// assert that the contents were written correctly
+	got := string(bytes)
+	if got != conf {
+		t.Errorf("failed to write configs: want: %s, got: %s", conf, got)
+	}
+}
