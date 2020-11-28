a58294859e2381e26f3fcf772da0a376069b5872
Chris Lane <chris@chris-allen-lane.com>
2020-11-27 22:26:14 -0500

    chore: spelling
    `s/pathSheets/pathsheets/g` in `cmd_list` for consistency elsewhere.

--- cmd/cheat/cmd_list.go
+++ cmd/cheat/cmd_list.go
@@ -37,8 +37,8 @@ func cmdList(opts map[string]interface{}, conf config.Config) {
 	// sheets with local sheets), here we simply want to create a slice
 	// containing all sheets.
 	flattened := []sheet.Sheet{}
-	for _, pathSheets := range cheatsheets {
-		for _, s := range pathSheets {
+	for _, pathsheets := range cheatsheets {
+		for _, s := range pathsheets {
 			flattened = append(flattened, s)
 		}
 	}
